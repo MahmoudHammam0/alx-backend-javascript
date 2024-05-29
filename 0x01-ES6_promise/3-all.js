da65c8f2eb09ac7d588ae297335b502158a4c3c5
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-05-29 17:01:53 +0000

    3. Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default async function handleProfileSignup() {
+export default function handleProfileSignup() {
   const promise1 = uploadPhoto();
   const promise2 = createUser();
+
   return Promise.all([promise1, promise2]).then((data) => {
-    console.log(data[0].body, data[1].firstName, data[1].lastName);
+    console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
   })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+    .catch(() => { console.log('Signup system offline'); });
 }
