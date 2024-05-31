ab9b61a621a88f71bcf6435d607ad7da43ceb6d1
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-05-31 00:18:44 +0000

    4. Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,32 @@
+import Currency from './3-currency'; // eslint-disable-line no-unused-vars
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return (`${this._amount} ${this._currency._name} (${this._currency._code})`);
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return (amount * conversionRate);
+  }
+}
