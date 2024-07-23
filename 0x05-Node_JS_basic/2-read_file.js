7044a09447e8760d4638da17d590dc0a6f470c2c
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-07-23 11:37:13 +0000

    2. Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,35 +1,32 @@
 const fs = require('fs');
-const path = require('path');
 
 const countStudents = (thePath) => {
-  const filePath = path.join(__dirname, thePath);
-  try {
-    const data = fs.readFileSync(filePath, 'utf8');
-    const dataList = data.trim().split('\n');
-    const students = dataList.slice(1);
-    console.log('Number of students:', students.length);
-    const mainDict = {
-      CS: { Count: 0, List: [] },
-      SWE: { Count: 0, List: [] },
-    };
-    students.forEach((student) => {
-      const studentInfo = student.split(',');
-      const name = studentInfo[0];
-      const field = studentInfo[studentInfo.length - 1];
-      if (mainDict[field]) {
-        mainDict[field].Count += 1;
-        mainDict[field].List.push(name);
-      }
-    });
-    for (const key in mainDict) {
-      if (Object.prototype.hasOwnProperty.call(mainDict, key)) {
-        const count = mainDict[key].Count;
-        const names = mainDict[key].List.join(', ');
-        console.log(`Number of students in ${key}: ${count}. List: ${names}`);
-      }
-    }
-  } catch (err) {
+  if (!fs.existsSync(thePath)) {
     throw new Error('Cannot load the database');
+  }
+  const data = fs.readFileSync(thePath, 'utf8');
+  const dataList = data.trim().split('\n');
+  const students = dataList.slice(1);
+  console.log('Number of students:', students.length);
+  const mainDict = {
+    CS: { Count: 0, List: [] },
+    SWE: { Count: 0, List: [] },
+  };
+  students.forEach((student) => {
+    const studentInfo = student.split(',');
+    const name = studentInfo[0];
+    const field = studentInfo[studentInfo.length - 1];
+    if (mainDict[field]) {
+      mainDict[field].Count += 1;
+      mainDict[field].List.push(name);
+    }
+  });
+  for (const key in mainDict) {
+    if (Object.prototype.hasOwnProperty.call(mainDict, key)) {
+      const count = mainDict[key].Count;
+      const names = mainDict[key].List.join(', ');
+      console.log(`Number of students in ${key}: ${count}. List: ${names}`);
+    }
   }
 };
 
