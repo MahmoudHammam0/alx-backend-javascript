22418cfb105309f5ac4fd1db2612992dbc4ef70e
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-07-23 11:41:18 +0000

    2. Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,44 +1,31 @@
 const fs = require('fs');
 
-const countStudents = (path) => {
-  if (!fs.existsSync(path)) {
+const countStudents = (thePath) => {
+  if (!fs.existsSync(thePath)) {
     throw new Error('Cannot load the database');
   }
-
-  const data = fs.readFileSync(path, 'utf8');
-  const lines = data.trim().split('\n'); // Split the data into lines
-  const students = lines.slice(1); // Remove the header line
-
-  const totalStudents = students.length; // Count the total number of students
-  console.log(`Number of students: ${totalStudents}`);
-
-  // Initialize an object to hold the count of students in each field
-  const fields = {};
-
-  students.forEach((line) => {
-    const parts = line.split(','); // Split each line by comma
-    const field = parts[3]; // The field is the fourth element in the array
-    const firstname = parts[0]; // The firstname is the first element in the array
-
-    // If the field doesn't exist in the fields object,
-    // initialize it as an empty array
-    if (!fields[field]) {
-      fields[field] = [];
+  const data = fs.readFileSync(thePath, 'utf8');
+  const dataList = data.trim().split('\n');
+  const students = dataList.slice(1);
+  console.log('Number of students:', students.length);
+  const mainDict = {
+    CS: { Count: 0, List: [] },
+    SWE: { Count: 0, List: [] },
+  };
+  students.forEach((student) => {
+    const studentInfo = student.split(',');
+    const name = studentInfo[0];
+    const field = studentInfo[studentInfo.length - 1];
+    if (mainDict[field]) {
+      mainDict[field].Count += 1;
+      mainDict[field].List.push(name);
     }
-    fields[field].push(firstname); // Add the firstname to the respective field
   });
-
-  // fields = {
-  //   CS: [ 'Johann', 'Arielle', 'Jonathan', 'Emmanuel', 'Guillaume', 'Katie' ],
-  //   SWE: [ 'Guillaume', 'Joseph', 'Paul', 'Tommy' ]
-  // }
-
-  for (const field in fields) {
-    // Make sure the 'fields' object has a property with the name of 'field'
-    if (Object.prototype.hasOwnProperty.call(fields, field)) {
-      const count = fields[field].length;
-      const names = fields[field].join(', ');
-      console.log(`Number of students in ${field}: ${count}. List: ${names}`);
+  for (const key in mainDict) {
+    if (Object.prototype.hasOwnProperty.call(mainDict, key)) {
+      const count = mainDict[key].Count;
+      const names = mainDict[key].List.join(', ');
+      console.log(`Number of students in ${key}: ${count}. List: ${names}`);
     }
   }
 };
