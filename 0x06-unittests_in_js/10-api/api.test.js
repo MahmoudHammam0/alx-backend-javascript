d85d7b33a4dba77d8c4f77c5a7fc112066645790
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-07-25 08:14:52 +0000

    10. Deep equality & Post integration testing

---
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -0,0 +1,38 @@
+const expect = require('chai').expect;
+const request = require('request');
+
+describe('index page testing', function() {
+  it('should respond successfully with welcome message', function() {
+    request('http://localhost:7865', (error, res, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      expect(res.statusCode).to.equal(200);
+      expect(res.request.method).to.equal('GET');
+    });
+  });
+
+  it('should respond successfully to num cart ids', function() {
+    request('http://localhost:7865/cart/12', (error, res, body) => {
+      expect(body).to.equal('Payment methods for cart 12');
+      expect(res.statusCode).to.equal(200);
+      expect(res.request.method).to.equal('GET');
+    });
+  });
+
+  it('should fail with strings as id for cart', function() {
+    request('http://localhost:7865/cart/hello', (error, res, body) => {
+      expect(res.statusCode).to.equal(404);
+    });
+  });
+
+  it('should fail with non numeric values as id for cart', function() {
+    request('http://localhost:7865/cart/d200-44a5-9de6', (error, res, body) => {
+      expect(res.statusCode).to.equal(404);
+    });
+  });
+
+  it('should fail with and object as id for cart', function() {
+    request('http://localhost:7865/cart/{"num": 5}', (error, res, body) => {
+      expect(res.statusCode).to.equal(404);
+    });
+  });
+});
