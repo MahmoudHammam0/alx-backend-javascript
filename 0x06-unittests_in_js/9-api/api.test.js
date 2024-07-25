2bf25c8346256abb63213ac47006b246afa3651b
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-07-25 07:06:49 +0000

    9. Regex integration testing

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -2,29 +2,26 @@ const expect = require('chai').expect;
 const request = require('request');
 
 describe('index page testing', function() {
-  it('should respond successfully with welcome message', function(done) {
+  it('should respond successfully with welcome message', function() {
     request('http://localhost:7865', (error, res, body) => {
       expect(body).to.equal('Welcome to the payment system');
       expect(res.statusCode).to.equal(200);
       expect(res.request.method).to.equal('GET');
-      done();
     });
   });
 
-  it('should respond successfully to num cart ids', function(done) {
+  it('should respond successfully to num cart ids', function() {
     request('http://localhost:7865/cart/12', (error, res, body) => {
       expect(body).to.equal('Payment methods for cart 12');
       expect(res.statusCode).to.equal(200);
       expect(res.request.method).to.equal('GET');
-      done();
     });
   });
 
-  it('should fail with strings as id for cart', function(done) {
+  it('should fail with strings as id for cart', function() {
     request('http://localhost:7865/cart/hello', (error, res, body) => {
       expect(res.statusCode).to.equal(404);
       expect(res.request.method).to.be.equal('GET');
-      done();
     });
   });
 });
