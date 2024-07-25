c7dfab99254170b4249ae84c4fe906ed0f9deb01
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-07-25 05:08:22 +0000

    4. Stubs

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -3,13 +3,6 @@ const sinon = require('sinon');
 const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', function() {
-  it('should call the payment function with the correct args with spies', function() {
-    const spy = sinon.spy(Utils, 'calculateNumber');
-    sendPaymentRequestToApi(100, 20);
-    spy.calledWith('SUM', 100, 20);
-    spy.restore();
-  });
-
   it('should call the payment function with the correct args with stubs', function() {
     const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
     const spy = sinon.spy(console, 'log');
