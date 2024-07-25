7557df5f3a7a77c4db649f0e722e00fea0d4018a
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-07-25 03:42:45 +0000

    3. Spies

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,19 +1,19 @@
-function calculateNumber(type, a, b) {
-  const roundA = Math.round(a);
-  const roundB = Math.round(b);
-  if (type === 'DIVIDE' && roundB === 0) {
-    return 'Error';
+const Utils = {
+  calculateNumber(type, a, b) {
+    const roundA = Math.round(a);
+    const roundB = Math.round(b);
+    if (type === 'DIVIDE' && roundB === 0) {
+      return 'Error';
+    }
+
+    const ops = {
+      'SUM': (a, b) => a + b,
+      'SUBTRACT': (a, b) => a - b,
+      'DIVIDE': (a, b) => a / b
+    }
+
+    return ops[type](roundA, roundB);
   }
-
-  const ops = {
-    'SUM': (a, b) => a + b,
-    'SUBTRACT': (a, b) => a - b,
-    'DIVIDE': (a, b) => a / b
-  }
-
-  return ops[type](roundA, roundB);
-}
-
-const Utils = { calculateNumber };
+};
 
 module.exports = Utils;
