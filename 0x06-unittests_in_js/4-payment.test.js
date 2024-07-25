26415499095f1ba3ed3a72311b6f23591bfb763a
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-07-25 04:26:21 +0000

    4. Stubs

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -16,7 +16,6 @@ describe('sendPaymentRequestToApi', function() {
     sendPaymentRequestToApi(100, 20);
     stub.calledWith('SUM', 100, 20);
     spy.calledWith('The total is: 10');
-    stub.returns(10);
     spy.restore();
     stub.restore();
   });
