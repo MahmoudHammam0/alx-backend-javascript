2d19683662e81a0688d299ee3e62c2cb40815988
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-07-25 00:41:01 +0000

    0. Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,4 +5,16 @@ describe('calculateNumber()', function() {
   it('should return sum of 2 rounded nums', function() {
     assert.equal(calculateNumber(2.6, 3.7), 7)
   });
+  it('should return sum of 2 rounded nums', function() {
+    assert.equal(calculateNumber(1, 3), 4)
+  });
+  it('should return sum of 2 rounded nums', function() {
+    assert.equal(calculateNumber(1, 3.7), 5)
+  });
+  it('should return sum of 2 rounded nums', function() {
+    assert.equal(calculateNumber(1.2, 3.7), 5)
+  });
+  it('should return sum of 2 rounded nums', function() {
+    assert.equal(calculateNumber(1.5, 3.7), 6)
+  });
 });
