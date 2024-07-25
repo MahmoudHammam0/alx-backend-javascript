bc72ac3361171b00af07d9d86583d1f09704e17b
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-07-25 03:45:13 +0000

    3. Spies

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,7 +1,7 @@
 const Utils = require('./utils');
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
-  totalPayment = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  const totalPayment = Utils.calculateNumber('SUM', totalAmount, totalShipping);
   console.log('The total is: %d', totalPayment);
 }
 
