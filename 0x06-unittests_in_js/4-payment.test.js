6255b442c3fcfc781d58a958bb373d0933662247
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-07-25 04:07:24 +0000

    4. Stubs

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,17 @@
+const expect = require('chai').expect;
+const sendPaymentRequestToApi = require('./3-payment');
+const sinon = require('sinon');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', function() {
+  it('should call the payment function with the correct args', function() {
+    const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const spy = sinon.spy(console, 'log');
+    sendPaymentRequestToApi(100, 20);
+    expect(stub.calledOnce).to.be.true;
+    expect(stub.calledWith('SUM', 100, 20)).to.be.true;
+    expect(spy.calledWith('The total is: 10')).to.be.true;
+    spy.restore();
+    stub.restore();
+  });
+});
