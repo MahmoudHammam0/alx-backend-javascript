d495410eab4fa0b2812731f2e9b66a204a91a90a
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-07-26 02:49:39 +0000

    8. Basic Integration testing

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -2,54 +2,61 @@ const expect = require('chai').expect;
 const request = require('request');
 
 describe('index page testing', function() {
-  it('should respond successfully with welcome message', function() {
+  it('should respond successfully with welcome message', function(done) {
     request('http://localhost:7865', (error, res, body) => {
       expect(body).to.equal('Welcome to the payment system');
       expect(res.statusCode).to.equal(200);
       expect(res.request.method).to.equal('GET');
+      done();
     });
   });
 
-  it('should respond successfully to num cart ids', function() {
+  it('should respond successfully to num cart ids', function(done) {
     request('http://localhost:7865/cart/12', (error, res, body) => {
       expect(body).to.equal('Payment methods for cart 12');
       expect(res.statusCode).to.equal(200);
       expect(res.request.method).to.equal('GET');
+      done();
     });
   });
 
-  it('should fail with strings as id for cart', function() {
+  it('should fail with strings as id for cart', function(done) {
     request('http://localhost:7865/cart/hello', (error, res, body) => {
       expect(res.statusCode).to.equal(404);
+      done();
     });
   });
 
-  it('should fail with non numeric values as id for cart', function() {
+  it('should fail with non numeric values as id for cart', function(done) {
     request('http://localhost:7865/cart/d200-44a5-9de6', (error, res, body) => {
       expect(res.statusCode).to.equal(404);
+      done();
     });
   });
 
-  it('should fail with and object as id for cart', function() {
+  it('should fail with and object as id for cart', function(done) {
     request('http://localhost:7865/cart/{"num": 5}', (error, res, body) => {
       expect(res.statusCode).to.equal(404);
+      done();
     });
   });
 
-it('should return the available payment object', function() {
+it('should return the available payment object', function(done) {
     request('http://localhost:7865/available_payments', (error, res, body) => {
       expect(res.statusCode).to.equal(200);
       expect(JSON.parse(body))
         .to.be.deep.equal({ payment_methods: {credit_cards: true, paypal: false} });
       expect(res.request.method).to.equal('GET');
+      done();
     });
   });
 
-  it('should return a welcome message with the sent userName', function() {
+  it('should return a welcome message with the sent userName', function(done) {
     request.post('http://localhost:7865/login', {json: {userName: 'Betty'}}, (error, res, body) => {
       expect(res.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Welcome Betty');
       expect(res.request.method).to.equal('POST');
+      done();
     });
   });
 });
