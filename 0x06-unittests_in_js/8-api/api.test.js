e178593ca674fdf6733e4ece0a00ecd1291c3c08
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-07-26 02:44:46 +0000

    8. Basic Integration testing

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -2,11 +2,12 @@ const expect = require('chai').expect;
 const request = require('request');
 
 describe('index page testing', function() {
-  it('should respond successfully with welcome message', function() {
+  it('should respond successfully with welcome message', function(done) {
     request('http://localhost:7865', (error, res, body) => {
       expect(body).to.equal('Welcome to the payment system');
       expect(res.statusCode).to.equal(200);
       expect(res.request.method).to.equal('GET');
+      done();
     });
   });
 });
