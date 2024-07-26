ff9cf48d9cc8fd392604a3890bb15a5e2aeb11ed
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-07-26 02:38:02 +0000

    4. Stubs

--- 0x06-unittests_in_js/4-payment.js
+++ 0x06-unittests_in_js/4-payment.js
@@ -1,8 +1,8 @@
-const Utils = require("./utils");
+const Utils = require('./utils');
 
 const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
-  const totalPayment = Utils.calculateNumber("SUM", totalAmount, totalShipping);
-  console.log(`The total is: ${totalPayment}`);
-}
+  const result = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${result}`);
+};
 
 module.exports = sendPaymentRequestToApi;
